a4dec09c88e87ac4da267cd8c45c696a30964899
gao-jinlong <2675130594@qq.com>
2023-10-31 00:13:44 +0800

    feat(nginx): 负载均衡

--- nginx-test/src/app.controller.ts
+++ nginx-test/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,7 +11,9 @@ export class AppController {
   }
 
   @Get('/api')
-  hello() {
+  hello(@Headers() headers) {
+    // console.log(headers);
+    console.log('access');
     return 'hello nginx!';
   }
 }
