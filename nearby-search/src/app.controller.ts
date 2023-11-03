f6b5b66984a9b595c9b478e4df6880adbb9dbb4d
gao-jinlong <2675130594@qq.com>
2023-11-04 01:15:46 +0800

    feat(nearby-search)

---
+++ nearby-search/src/app.controller.ts
@@ -0,0 +1,70 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Inject,
+  Query,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { RedisService } from './redis/redis.service';
+
+@Controller()
+export class AppController {
+  @Inject(RedisService)
+  private redisService: RedisService;
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get('addPos')
+  async addPos(
+    @Query('name') posName: string,
+    @Query('longitude') longitude: number,
+    @Query('latitude') latitude: number,
+  ) {
+    if (!posName || !longitude || !latitude) {
+      throw new BadRequestException('位置信息不全');
+    }
+    try {
+      await this.redisService.geoAdd('positions', posName, [
+        longitude,
+        latitude,
+      ]);
+
+      return {
+        message: '添加成功',
+        statusCode: 200,
+      };
+    } catch (e) {}
+  }
+
+  @Get('pos')
+  async pos(@Query('name') name: string) {
+    return this.redisService.geoPos('positions', name);
+  }
+
+  @Get('nearbySearch')
+  async nearbySearch(
+    @Query('longitude') longitude: number,
+    @Query('latitude') latitude: number,
+    @Query('radius') radius: number,
+  ) {
+    if (!longitude || !latitude) {
+      throw new BadRequestException('缺少位置信息');
+    }
+
+    if (!radius) {
+      throw new BadRequestException('缺少搜索半径');
+    }
+
+    return this.redisService.geoSearch(
+      'positions',
+      [longitude, latitude],
+      radius,
+    );
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
