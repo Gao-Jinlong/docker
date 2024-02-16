e3fab89001beb2407f53b089872ad575d1d46000
gao-jinlong <2675130594@qq.com>
2024-02-16 22:18:43 +0800

    feat(graphql)

--- graphql-crud-demo/index.js
+++ graphql-crud-demo/index.js
@@ -1,4 +1,5 @@
 import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `
   type Student {
@@ -13,12 +14,26 @@ const typeDefs = `
     name: String,
     age: Int,
     subject: [String],
-    students: [Student]
+    students: [Student],
   }
 
   type Query {
     students: [Student],
     teachers: [Teacher],
+    studentsbyTeacherName(name: String!): [Student]
+  }
+
+  type Res {
+    success: Boolean,
+    id: String
+  }
+
+  type Mutation {
+    addStudent(name: String! age: Int! sex: Boolean!): Res
+
+    updateStudent(id: String! name: String! age: Int! sex: Boolean!): Res
+
+    deleteStudent(id: String!): Res
   }
 
   schema {
@@ -65,6 +80,48 @@ const resolvers = {
   Query: {
     students: () => students,
     teachers: () => teachers,
+    studentsbyTeacherName: async (...args) => {
+      console.log(args);
+      const name = args[1].name;
+      return students.filter((student) => student.name === name);
+    },
+  },
+  Mutation: {
+    addStudent: addStudent,
+    updateStudent: updateStudent,
+    deleteStudent: deleteStudent,
   },
 };
-//test
+
+async function addStudent(_, { name, age, sex }) {
+  students.push({
+    id: students.length + 1,
+    name,
+    age,
+    sex,
+  });
+}
+
+async function updateStudent(_, { id, name, age, sex }) {
+  return {
+    success: true,
+    id,
+  };
+}
+
+async function deleteStudent(_, { id }) {
+  return {
+    success: true,
+    id,
+  };
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`🚀 Server ready at ${url}`);
