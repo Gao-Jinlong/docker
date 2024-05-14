a68dd6e72b109da5705eac9075b4eb9f86c61bba
Ginlon <2675130594@qq.com>
2024-05-15 00:34:10 +0800

    feat: follow

--- follow/src/app.module.ts
+++ follow/src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
@@ -14,13 +17,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'following_test',
       synchronize: true,
       logging: true,
-      entities: [],
+      entities: [User],
       poolSize: 10,
       connectorPackage: 'mysql2',
       extra: {
         authPlugins: 'sha256_password',
       },
     }),
+    UserModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
